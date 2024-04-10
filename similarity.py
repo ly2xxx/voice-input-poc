ee8d30e71cb4653fbea242884259337c79961162
ly2xxx <ly2xxx@hotmail.com>
2024-04-11 00:14:06 +0100

    Fully working similarity calculator

--- similarity.py
+++ similarity.py
@@ -1,49 +1,12 @@
 import streamlit as st
 from modules.similarity import SimilarityScorer
 import decimal
-import speech_recognition as sr
-
-def listen_and_transcribe():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        audio = r.listen(source)
-        
-    try:
-        text = r.recognize_google(audio)
-        return text
-    except:
-        return ""
+from streamlit_mic_recorder import speech_to_text
 
 st.title("Text Similarity Calculator")
 
-if "secondText" not in st.session_state:
-    st.session_state["secondText"] = ""
-
 text1 = st.text_area("Enter first text:", "")
-text2 = st.text_area("Enter second text:", value=st.session_state["secondText"])
-
-# Initialize the recognizer
-r = sr.Recognizer()
-if st.button("Listen"):
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source)
-        st.write("Listening... Speak now!")
-        try:
-            # Listen for audio input
-            audio = r.listen(source, timeout=5)
-            st.write("Processing...")
-
-            # Recognize the speech
-            text = r.recognize_google(audio)
-            st.write(f"Recognized text: {text}")
-            st.session_state["secondText"] = text
-        except sr.WaitTimeoutError:
-            st.write("Timeout: No speech detected.")
-        except sr.UnknownValueError:
-            st.write("Error: Unable to recognize speech.")
-        except sr.RequestError as e:
-            st.write(f"Error: {e}")
-
+text2 = st.text_area("Enter second text:", "")
 
 scorer = SimilarityScorer() 
     
@@ -58,3 +21,10 @@ if st.button("Score"):
     # Format to two decimal places  
     percentage = '{0:.2f}%'.format(score_formatted)
     st.write("Similarity score:", percentage)
+
+def callback():
+    if st.session_state.my_stt_output:
+        st.text_area("Copy recorded text to above area 👆", st.session_state.my_stt_output)
+
+
+speech_to_text(key='my_stt', callback=callback)    
